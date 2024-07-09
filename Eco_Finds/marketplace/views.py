8b88e0b801f222be99a630ff2beac1d448e9b4bd
Rishit Bhojak <rajabhojak@gmail.com>
2024-07-09 17:02:10 -0400

    Cart: Added like product handler and add/remove product functionality

--- Eco_Finds/marketplace/views.py
+++ Eco_Finds/marketplace/views.py
@@ -225,3 +225,25 @@ def rewards(request):
 #         form = RewardForm()
 #     return render(request, 'marketplace/add_reward.html', {'form': form})
 # return render(request, 'marketplace/order_success.html', {'username': request.session.get('username')})
+@login_required
+def view_cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_items = cart.items.all()
+    return render(request, 'cart.html', {'cart_items': cart_items})
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('view_cart')
+
+@login_required
+def remove_from_cart(request, product_id):
+    cart = get_object_or_404(Cart, user=request.user)
+    cart_item = get_object_or_404(CartItem, cart=cart, product__id=product_id)
+    cart_item.delete()
+    return redirect('view_cart')
